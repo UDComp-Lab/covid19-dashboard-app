6733095b8d3c4d2e7c2f59b4ef499b11a2a77df5
Owen Bartolf <owen.bartolf@Gmail.com>
2020-03-24 14:31:43 -0700

    Built a very simple parameter validator.

---
+++ backend/lib/Validation/validateRestifyReqJson.js
@@ -0,0 +1,33 @@
+/*
+    validateRestifyReqJson.js
+
+    This is a simple function that can be passed a req and an object with
+    a bunch of required field and type pairs to ensure the request has them
+    in the body.
+*/
+
+module.exports = function(restifyReq, requiredFieldTypePairs)
+{
+    // Fail if no body provided.
+    if (restifyReq.body == null)
+    {
+        return false;
+    }
+
+    // Check if body has every value
+    for (const [key, requiredType] of Object.entries(requiredFields))
+    {
+
+        if (
+            restifyReq.body[key] == null ||
+            typeof(restifyReq.body[key]) !== requiredType
+        )
+        {
+            return false;
+        }
+
+    }
+
+
+    return true; // Passed validation
+}
